6a8e53c9e9ceaf44444ef6e733ae9740f61332fa
Artem <egoridze05@gmail.com>
2024-02-14 20:57:43 +0200

    влез в абойт форм

--- MatematicoGame/AuthUserForm.Designer.cs
+++ MatematicoGame/AuthUserForm.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AuthUserForm";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 80F);
+            label1.Location = new Point(43, 128);
+            label1.Name = "label1";
+            label1.Size = new Size(712, 142);
+            label1.TabIndex = 0;
+            label1.Text = "ПРИВЕТ МИР";
+            // 
+            // AuthUserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "AuthUserForm";
+            Text = "AuthUserForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
