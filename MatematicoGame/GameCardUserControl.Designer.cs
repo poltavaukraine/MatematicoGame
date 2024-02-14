f54908960cdec0ce9ae3b212451b223675d41250
Roma <roman@gmail.com>
2024-02-14 20:06:47 +0200

    added Game Card User Control

---
+++ MatematicoGame/GameCardUserControl.Designer.cs
@@ -0,0 +1,443 @@
+﻿namespace MatematicoGame
+{
+    partial class GameCardUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel = new TableLayoutPanel();
+            button_1 = new Button();
+            button_2 = new Button();
+            button_3 = new Button();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button_4 = new Button();
+            button_5 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button22 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            tableLayoutPanel.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            tableLayoutPanel.ColumnCount = 5;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.Controls.Add(button24, 4, 4);
+            tableLayoutPanel.Controls.Add(button23, 3, 4);
+            tableLayoutPanel.Controls.Add(button22, 2, 4);
+            tableLayoutPanel.Controls.Add(button21, 1, 4);
+            tableLayoutPanel.Controls.Add(button20, 0, 4);
+            tableLayoutPanel.Controls.Add(button19, 4, 3);
+            tableLayoutPanel.Controls.Add(button18, 3, 3);
+            tableLayoutPanel.Controls.Add(button17, 2, 3);
+            tableLayoutPanel.Controls.Add(button16, 1, 3);
+            tableLayoutPanel.Controls.Add(button15, 0, 3);
+            tableLayoutPanel.Controls.Add(button14, 4, 2);
+            tableLayoutPanel.Controls.Add(button13, 3, 2);
+            tableLayoutPanel.Controls.Add(button12, 2, 2);
+            tableLayoutPanel.Controls.Add(button11, 1, 2);
+            tableLayoutPanel.Controls.Add(button10, 0, 2);
+            tableLayoutPanel.Controls.Add(button9, 4, 1);
+            tableLayoutPanel.Controls.Add(button8, 3, 1);
+            tableLayoutPanel.Controls.Add(button7, 2, 1);
+            tableLayoutPanel.Controls.Add(button6, 1, 1);
+            tableLayoutPanel.Controls.Add(button5, 0, 1);
+            tableLayoutPanel.Controls.Add(button_5, 4, 0);
+            tableLayoutPanel.Controls.Add(button_4, 3, 0);
+            tableLayoutPanel.Controls.Add(button_3, 2, 0);
+            tableLayoutPanel.Controls.Add(button_2, 1, 0);
+            tableLayoutPanel.Controls.Add(button_1, 0, 0);
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.Location = new Point(0, 0);
+            tableLayoutPanel.Name = "tableLayoutPanel";
+            tableLayoutPanel.RowCount = 5;
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel.Size = new Size(500, 500);
+            tableLayoutPanel.TabIndex = 0;
+            // 
+            // button_1
+            // 
+            button_1.Dock = DockStyle.Fill;
+            button_1.Font = new Font("Segoe UI", 36F);
+            button_1.Location = new Point(3, 3);
+            button_1.Name = "button_1";
+            button_1.Size = new Size(94, 94);
+            button_1.TabIndex = 0;
+            button_1.UseVisualStyleBackColor = true;
+            // 
+            // button_2
+            // 
+            button_2.Dock = DockStyle.Fill;
+            button_2.Font = new Font("Segoe UI", 36F);
+            button_2.Location = new Point(103, 3);
+            button_2.Name = "button_2";
+            button_2.Size = new Size(94, 94);
+            button_2.TabIndex = 1;
+            button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_3
+            // 
+            button_3.Dock = DockStyle.Fill;
+            button_3.Font = new Font("Segoe UI", 36F);
+            button_3.Location = new Point(203, 3);
+            button_3.Name = "button_3";
+            button_3.Size = new Size(94, 94);
+            button_3.TabIndex = 2;
+            button_3.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Controls.Add(button1, 2, 0);
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(200, 100);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Font = new Font("Segoe UI", 36F);
+            button1.Location = new Point(83, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(34, 94);
+            button1.TabIndex = 2;
+            button1.Text = "13";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Segoe UI", 36F);
+            button2.Location = new Point(43, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(34, 94);
+            button2.TabIndex = 1;
+            button2.Text = "13";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button_4
+            // 
+            button_4.Dock = DockStyle.Fill;
+            button_4.Font = new Font("Segoe UI", 36F);
+            button_4.Location = new Point(303, 3);
+            button_4.Name = "button_4";
+            button_4.Size = new Size(94, 94);
+            button_4.TabIndex = 3;
+            button_4.UseVisualStyleBackColor = true;
+            // 
+            // button_5
+            // 
+            button_5.Dock = DockStyle.Fill;
+            button_5.Font = new Font("Segoe UI", 36F);
+            button_5.Location = new Point(403, 3);
+            button_5.Name = "button_5";
+            button_5.Size = new Size(94, 94);
+            button_5.TabIndex = 4;
+            button_5.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Fill;
+            button5.Font = new Font("Segoe UI", 36F);
+            button5.Location = new Point(3, 103);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 94);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Dock = DockStyle.Fill;
+            button6.Font = new Font("Segoe UI", 36F);
+            button6.Location = new Point(103, 103);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 94);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Dock = DockStyle.Fill;
+            button7.Font = new Font("Segoe UI", 36F);
+            button7.Location = new Point(203, 103);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 94);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Dock = DockStyle.Fill;
+            button8.Font = new Font("Segoe UI", 36F);
+            button8.Location = new Point(303, 103);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 94);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Dock = DockStyle.Fill;
+            button9.Font = new Font("Segoe UI", 36F);
+            button9.Location = new Point(403, 103);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 94);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Dock = DockStyle.Fill;
+            button10.Font = new Font("Segoe UI", 36F);
+            button10.Location = new Point(3, 203);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 94);
+            button10.TabIndex = 10;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Dock = DockStyle.Fill;
+            button11.Font = new Font("Segoe UI", 36F);
+            button11.Location = new Point(103, 203);
+            button11.Name = "button11";
+            button11.Size = new Size(94, 94);
+            button11.TabIndex = 11;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Dock = DockStyle.Fill;
+            button12.Font = new Font("Segoe UI", 36F);
+            button12.Location = new Point(203, 203);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 94);
+            button12.TabIndex = 12;
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Dock = DockStyle.Fill;
+            button13.Font = new Font("Segoe UI", 36F);
+            button13.Location = new Point(303, 203);
+            button13.Name = "button13";
+            button13.Size = new Size(94, 94);
+            button13.TabIndex = 13;
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Dock = DockStyle.Fill;
+            button14.Font = new Font("Segoe UI", 36F);
+            button14.Location = new Point(403, 203);
+            button14.Name = "button14";
+            button14.Size = new Size(94, 94);
+            button14.TabIndex = 14;
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Dock = DockStyle.Fill;
+            button15.Font = new Font("Segoe UI", 36F);
+            button15.Location = new Point(3, 303);
+            button15.Name = "button15";
+            button15.Size = new Size(94, 94);
+            button15.TabIndex = 15;
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Dock = DockStyle.Fill;
+            button16.Font = new Font("Segoe UI", 36F);
+            button16.Location = new Point(103, 303);
+            button16.Name = "button16";
+            button16.Size = new Size(94, 94);
+            button16.TabIndex = 16;
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            button17.Dock = DockStyle.Fill;
+            button17.Font = new Font("Segoe UI", 36F);
+            button17.Location = new Point(203, 303);
+            button17.Name = "button17";
+            button17.Size = new Size(94, 94);
+            button17.TabIndex = 17;
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Dock = DockStyle.Fill;
+            button18.Font = new Font("Segoe UI", 36F);
+            button18.Location = new Point(303, 303);
+            button18.Name = "button18";
+            button18.Size = new Size(94, 94);
+            button18.TabIndex = 18;
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Dock = DockStyle.Fill;
+            button19.Font = new Font("Segoe UI", 36F);
+            button19.Location = new Point(403, 303);
+            button19.Name = "button19";
+            button19.Size = new Size(94, 94);
+            button19.TabIndex = 19;
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Dock = DockStyle.Fill;
+            button20.Font = new Font("Segoe UI", 36F);
+            button20.Location = new Point(3, 403);
+            button20.Name = "button20";
+            button20.Size = new Size(94, 94);
+            button20.TabIndex = 20;
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            button21.Dock = DockStyle.Fill;
+            button21.Font = new Font("Segoe UI", 36F);
+            button21.Location = new Point(103, 403);
+            button21.Name = "button21";
+            button21.Size = new Size(94, 94);
+            button21.TabIndex = 21;
+            button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            button22.Dock = DockStyle.Fill;
+            button22.Font = new Font("Segoe UI", 36F);
+            button22.Location = new Point(203, 403);
+            button22.Name = "button22";
+            button22.Size = new Size(94, 94);
+            button22.TabIndex = 22;
+            button22.UseVisualStyleBackColor = true;
+            // 
+            // button23
+            // 
+            button23.Dock = DockStyle.Fill;
+            button23.Font = new Font("Segoe UI", 36F);
+            button23.Location = new Point(303, 403);
+            button23.Name = "button23";
+            button23.Size = new Size(94, 94);
+            button23.TabIndex = 23;
+            button23.UseVisualStyleBackColor = true;
+            // 
+            // button24
+            // 
+            button24.Dock = DockStyle.Fill;
+            button24.Font = new Font("Segoe UI", 36F);
+            button24.Location = new Point(403, 403);
+            button24.Name = "button24";
+            button24.Size = new Size(94, 94);
+            button24.TabIndex = 24;
+            button24.UseVisualStyleBackColor = true;
+            // 
+            // GameCardUserControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(tableLayoutPanel);
+            Name = "GameCardUserControl";
+            Size = new Size(500, 500);
+            tableLayoutPanel.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel;
+        private Button button24;
+        private Button button23;
+        private Button button22;
+        private Button button21;
+        private Button button20;
+        private Button button19;
+        private Button button18;
+        private Button button17;
+        private Button button16;
+        private Button button15;
+        private Button button14;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button_5;
+        private Button button_4;
+        private Button button_3;
+        private Button button_2;
+        private Button button_1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+    }
+}
